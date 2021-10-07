069453a725f4514e871d321c10debb15bcd22bf4
MIRANDA <miranda.zermeno4@gmail.com>
2021-10-07 00:58:05 -0500

    moved index.js outside of assets

---
+++ index.js
@@ -0,0 +1,22 @@
+// External & Internal Packages 
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// Need to include variables for employess
+
+
+
+
+// Questions to user//
+// manager's name
+// employee ID 
+// email address
+//office number 
+
+// present with a menu w/ the option to add an engineer or an intern or finish building my team 
+
+// select engineer option 
+// prompt to enter : engineer's name, ID, email, and GitHub username 
+//taken back to the menu
+
+// same thing for intern
