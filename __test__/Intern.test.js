a151b09d73d86ae21d997c0d1ba04d5d2f4b75c2
MIRANDA <miranda.zermeno4@gmail.com>
2021-10-14 21:24:55 -0500

    wcont working on tests

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -1,5 +1,22 @@
 const Intern= require ("./lib/Intern");
 
+describe("Can set school via constructor", () => {
+    const iTest = "SMU";
+    const employee = new Intern("Miranda", 4, "email.4@gmail.com", iTest);
+    expect(employee.school).toBe(iTest);
+  });
+  
+  it("getRole() should return \"Intern\"", () => {
+    const iTest = "Intern";
+    const employee = new Intern("Miranda", 4, "email.4@gmail.com", "SMU");
+    expect(employee.getRole()).toBe(iTest);
+  });
+  
+  it("Can get school via getSchool()", () => {
+    const iTest = "SMU";
+    const employee = new Intern("Miranda", 4, "email.4@gmail.com", iTest);
+    expect(employee.getSchool()).toBe(iTest);
+  });
 
 //test for school 
 
