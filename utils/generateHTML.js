5ed9bb08b03bc5fb39ce7919d8fba59132daffa1
MIRANDA <miranda.zermeno4@gmail.com>
2021-10-16 20:11:04 -0500

    revsied html

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -1,35 +1,78 @@
-// External and Internal packages 
-const inquirer = require ("inquirer");
-const fs = require("fs");
-const path = require("path");
-const Manager = require("../lib/Manager");
-const Engineer = require("../lib/Engineer");
-const Intern = require("../lib/Intern");
-
-//  HTML webpage 
-function generateHTML( input){
-    console.log( input.role);
-    return `
-    
-    <!DOCTYPE html>
+function generateHTML() {
+    return `<!DOCTYPE html>
     <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <!--BOOTSTRAP LINK  -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="styles.css">
+      <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" href="../dist/style.css">
         <title>TEAM-PRO-GEN</title>
+
     </head>
+    <div class="container">
+              <div class="row justify-content-center" id="team-cards">
     <body>
-        <header class='banner valign-wrapper '>
-            <h1 class='center-align'>Nakama Info Generator</h1>
-        </header>
-        <div class="container">`;
+   < div class="card">
+    <div class="card-header">
+     Welcome!
+    </div>
+    <div class="card-body">
+      <h1 class="card-title">Employee information </h1> 
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"></script>
+</body>
+</html>`;
+};
 
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"></script>
-}
+function generateManager(input){
+    return `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+            <h2>${input.name}</h2>
+            <h3>${input.getRole()}</h3>
+        </div>
+        <div class="card-text">
+            <li>ID: ${input.id}</li>
+            <li>Email: mailto: ${input.email}</li>
+            <li>Office number: ${input.officeNumber}</li>
+        </div>
+    </div>
+</div>`;
+};
+
+function generateEngineer(input){
+    return `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+            <h2>${input.name}</h2>
+            <h3>${input.getRole()}</h3>
+        </div>
+        <div class="card-text">
+            <li>ID: ${input.id}</li>
+            <li>Email: mailto: ${input.email}</li>
+            <li>GitHub: ${input.github}</li>
+        </div>
+    </div>
+</div>`;
+};
+
+function generateIntern(input) {
+    return `<div class="card" style="width: 18rem;>
+    <div class="card-body">
+            <h2>${input.name}</h2>
+            <h3>${input.getRole()}</h3>
+        </div>
+        <div class="card-text">
+            <li>ID: ${input.id}</li>
+            <li>Email: mailto: ${input.email}</li>
+            <li>School: ${input.school}</li>
+        </div>
+    </div>
+</div>`;
+};
 
 
-module.exports = generateHTML;
+
+module.exports = generateHTML,generateManager, generateEngineer,generateIntern;
